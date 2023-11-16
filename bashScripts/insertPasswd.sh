44fe04e1c6cb4ce4ace2913ab0d04b68c3d591ab
Quinn Ferguson <quinnferguson48@gmail.com>
2023-11-16 13:11:12 -0500

    added qol features

--- bashScripts/insertPasswd.sh
+++ bashScripts/insertPasswd.sh
@@ -10,7 +10,10 @@ if [ -z "$2" ]
   then
 	passwd=$(~/passwordManager/passgen)
 	echo "$1		$passwd" >> ~/passwordManager/passwords
-	echo $passwd
+	echo $passwd | clip.exe
+	echo "New password: $passwd"
+	echo "Copied to clipboard."
 else
 	echo "$1		$2" >> ~/passwordManager/passwords
+	echo "Password added."
 fi
