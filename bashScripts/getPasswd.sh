0121fce67207a8ddc1ee515ed22d245cefc24bf2
Quinn Ferguson <quinnferguson48@gmail.com>
2024-03-13 12:13:12 -0400

    Added QOL features to getpasswd function

--- bashScripts/getPasswd.sh
+++ bashScripts/getPasswd.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-  then
+if [ -z "$1" ]; then
 	cat ~/passwordManager/passwords
 else
 	grep "$1" ~/passwordManager/passwords
+	resultCount=$( grep -c "$1" ~/passwordManager/passwords )
+
+	if [ "$resultCount" -eq 0 ]; then
+		echo "No results."
+	elif [ "$resultCount" -lt 2 ]; then
+		#clip
+		grep "$1" ~/passwordManager/passwords | sed 's/^.*\t\t//' | clip.exe
+		echo "Password copied to clipboard."
+	fi
 fi
 
 
